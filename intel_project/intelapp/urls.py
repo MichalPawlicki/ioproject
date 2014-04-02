fbc90aec01bd0e20fe1b4880e77d01ec0cdc4657
Michal Pawlicki <mic.pawlicki@gmail.com>
2014-04-02 17:18:17 +0200

    Add registration-related urls.

--- intel_project/intelapp/urls.py
+++ intel_project/intelapp/urls.py
@@ -1,3 +1,5 @@
+from intelapp.views import RegisterView
+
 __author__ = 'michal'
 from django.conf.urls import patterns, url
 from intelapp import views
@@ -5,6 +7,8 @@ from intelapp import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<number>\d+)$', views.index_with_number, name='index_with_number')
+    url(r'^(?P<number>\d+)$', views.index_with_number, name='index_with_number'),
+    url(r'^register/?$', RegisterView.as_view(), name='register'),
+    url(r'^register/confirm/(?P<code>\w{128})$', views.confirm_registration, name='confirm_registration'),
 )
 
