882f192da8df1f4b07041336390b174709ac80d3
Michal Pawlicki <mic.pawlicki@gmail.com>
2014-04-02 19:19:52 +0200

    Create registration-related views.

--- intel_project/intelapp/views.py
+++ intel_project/intelapp/views.py
@@ -1,7 +1,21 @@
+import hashlib
+import random
+import string
+from django.core.mail import send_mail
+from django.db import transaction
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.http.response import Http404
 from django.shortcuts import render
 
+from django.template import RequestContext, loader
+
 # Create your views here.
+from django.views.decorators.http import require_GET
+from django.views.generic import FormView
+from intelapp import utils
+from intelapp.forms import RegisterForm
+from intelapp.models import UserProfile, UserGroup, RegistrationManager
 
 
 def index(request):
@@ -10,3 +24,26 @@ def index(request):
 
 def index_with_number(request, number):
     return HttpResponse(str(number))
+
+
+class RegisterView(FormView):
+    template_name = 'intelapp/register.html'
+    form_class = RegisterForm
+    success_url = '/intel/'
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        email = form.cleaned_data['email']
+        group = form.cleaned_data['group']
+        device_id = form.cleaned_data['device_id']
+        RegistrationManager.create_inactive_user(username, email, password, group, device_id)
+        return super(RegisterView, self).form_valid(form)
+
+
+def confirm_registration(request, code):
+    activated_user = RegistrationManager.activate_user(code)
+    if not activated_user:
+        return HttpResponse('Error when activating user.')
+    username = activated_user.username
+    return HttpResponse('User "{0}" activated!'.format(username))
