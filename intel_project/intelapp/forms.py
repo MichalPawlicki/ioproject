053f6c4e78df2e6ccd903905320acce5718dd5c4
Michal Pawlicki <mic.pawlicki@gmail.com>
2014-04-02 17:15:01 +0200

    Provide additional validation for RegisterForm.

--- intel_project/intelapp/forms.py
+++ intel_project/intelapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 from django.forms.widgets import PasswordInput
 
 __author__ = 'michal'
@@ -11,3 +12,18 @@ class RegisterForm(forms.Form):
     email = forms.EmailField()
     group = forms.CharField()
     device_id = forms.CharField(required=False)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username):
+            raise forms.ValidationError(
+                'User with provided username already exists.')
+        return username
+
+    def clean(self):
+        cleaned_data = super(RegisterForm, self).clean()
+        password = cleaned_data.get('password')
+        confirmed_password = cleaned_data.get('confirm_password')
+        if password != confirmed_password:
+            raise forms.ValidationError('Passwords do not match.')
+        return cleaned_data
